aa0e52e5f87eb8a491f9955209c86dda21bc3b7d
shutaro <145909040+s-shutaro@users.noreply.github.com>
2024-09-01 09:50:26 +0000

    ベース作成

---
+++ components/Calender.vue
@@ -0,0 +1,17 @@
+<template>
+<el-calendar v-model="value">
+</el-calendar>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Calender extends Vue {
+    get value(){
+        return new Date()
+    }
+}
+</script>
+
+<style src="../css/styles.css"></style>
---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    <el-row>
+      <el-col :span="24">
+        <h1>ヘッダー</h1>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HeaderComponent extends Vue {}
+
+</script>
+
+<style src="../css/styles.css"></style>
